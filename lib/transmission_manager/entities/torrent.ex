0d4dee94cf891fd57a71fe8eb345b7a2dad673ad
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-02-12 16:48:35 +0100

    wip commit

--- lib/transmission_manager/entities/torrent.ex
+++ lib/transmission_manager/entities/torrent.ex
@@ -1,26 +1,4 @@
-defmodule TransmissionManager.Tracker do
-  @moduledoc """
-  Model of a transmission tracker.
-  """
-  alias __MODULE__
 
-  @type t :: %__MODULE__{
-          :id => integer(),
-          :announce => String.t(),
-          :scrape => String.t(),
-          :tier => integer(),
-          :sitename => String.t()
-        }
-  @keys [
-    :id,
-    :announce,
-    :scrape,
-    :tier,
-    :sitename
-  ]
-  @enforce_keys @keys
-  defstruct @keys
-end
 
 defmodule TransmissionManager.Torrent do
   @moduledoc """
