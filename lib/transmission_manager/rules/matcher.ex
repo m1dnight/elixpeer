de8a6a74118624de689fe8aecd3695d2702635bc
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-07-27 14:25:15 +0200

    wip commit

--- lib/transmission_manager/rules/matcher.ex
+++ lib/transmission_manager/rules/matcher.ex
@@ -4,23 +4,13 @@ defmodule TransmissionManager.Rules.Matcher do
   alias TransmissionManager.Torrent
 
   @spec matches?(Torrent.t(), RuleSet.t() | Rule.t()) :: boolean()
-  def matches?(torrent, rule = %RuleSet{combinator: :and}) do
-    matches?(torrent, rule.left) and matches?(torrent, rule.right)
+  def matches?(torrent, rule = %RuleSet{}) do
+    rule.combinator.(matches?(torrent, rule.left), matches?(torrent, rule.right))
   end
 
-  def matches?(torrent, rule = %RuleSet{combinator: :or}) do
-    matches?(torrent, rule.left) or matches?(torrent, rule.right)
-  end
-
-  def matches?(torrent, rule = %Rule{operator: :=, field: field, value: value}) do
-    Map.get(torrent, field) == value
-  end
-
-  def matches?(torrent, rule = %Rule{operator: :>, field: field, value: value}) do
-    Map.get(torrent, field) > value
-  end
-
-  def matches?(torrent, rule = %RuleSet{combinator: :and}) do
-    matches?(torrent, rule.left) and matches?(torrent, rule.right)
+  def matches?(torrent, rule = %Rule{}) do
+    torrent_value = Map.get(torrent, rule.field)
+    expected_value = rule.value
+    rule.operator.(expected_value, torrent_value)
   end
 end
