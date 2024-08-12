ea6ca236071d11059945822fd20a74a7d950bcae
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-08-12 16:06:22 +0200

    add materialized view updater

---
+++ priv/repo/migrations/20240812140550_update_view.exs
@@ -0,0 +1,12 @@
+defmodule Elixpeer.Repo.Migrations.UpdateView do
+  use Ecto.Migration
+
+  def change do
+    execute """
+      SELECT add_continuous_aggregate_policy('activity_per_hour_per_torrent',
+    start_offset => INTERVAL '1 month',
+    end_offset => INTERVAL '1 hour',
+    schedule_interval => INTERVAL '1 hour');
+    """
+  end
+end
