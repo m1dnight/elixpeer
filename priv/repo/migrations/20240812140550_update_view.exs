b7f21d89b8c3c9baeafb927c1973d5c29e35b784
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-08-19 11:58:35 +0200

    formatting

--- priv/repo/migrations/20240812140550_update_view.exs
+++ priv/repo/migrations/20240812140550_update_view.exs
@@ -3,10 +3,10 @@ defmodule Elixpeer.Repo.Migrations.UpdateView do
 
   def change do
     execute """
-      SELECT add_continuous_aggregate_policy('activity_per_hour_per_torrent',
-    start_offset => INTERVAL '1 month',
-    end_offset => INTERVAL '1 hour',
-    schedule_interval => INTERVAL '1 hour');
+    SELECT add_continuous_aggregate_policy('activity_per_hour_per_torrent',
+                                          start_offset => INTERVAL '1 month',
+                                          end_offset => INTERVAL '1 hour',
+                                          schedule_interval => INTERVAL '1 hour');
     """
   end
 end
