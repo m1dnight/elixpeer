d0cf20ff473f1bfd95926a5b99ffdc2c43515d77
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-08-27 09:01:02 +0200

    messags in startpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@ bin="/app/bin/server"
 
 
 # Setup the database.
+echo "running migrations"
 $bin eval "Elixpeer.Release.migrate"
 
 # start the elixir application
+echo "starting application"
 exec "$bin"
